cbdc260be2ae7d9aa3611e4076de20bb6583ea36
Ebram Ayman <ebramayman55@gmail.com>
2026-02-25 23:36:34 +0200

    create hero-section and add button ui from shadcn

--- src/features/home/pages/HomePage.jsx
+++ src/features/home/pages/HomePage.jsx
@@ -1,3 +1,10 @@
+import HeroSection from "../components/HeroSection";
+
 export default function HomePage() {
-  return <div>Home Page</div>;
+  return (
+    <div className="h-lvh">
+      Home Page
+      <HeroSection />
+    </div>
+  );
 }
