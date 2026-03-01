63018408870d381f3fb2bc196db1b19f72bd8a53
Ebram Ayman <ebramayman55@gmail.com>
2026-03-01 20:01:45 +0200

    add animation to homepage

--- src/features/home/components/AboutSection.jsx
+++ src/features/home/components/AboutSection.jsx
@@ -3,6 +3,7 @@ import yearsEx from "../../../assets/images/Homepage/aboutSection/experience yea
 import calendar from "../../../assets/images/Homepage/aboutSection/calendar.png";
 import people from "../../../assets/images/Homepage/aboutSection/people.png";
 import institutions from "../../../assets/images/Homepage/aboutSection/Institutions.png";
+import ScrollAnimation from "@/components/ui/ScrollAnimation";
 
 const statsData = [
   { id: 1, icon: yearsEx, number: "2+", text: "Years Experience" },
@@ -14,40 +15,45 @@ const statsData = [
 export default function AboutSection() {
   return (
     <div className="flex flex-col items-center w-full gap-8 px-4 my-10 md:px-20 ">
-      <div className="flex flex-col items-center w-full p-8 text-center shadow-lg rounded-2xl md:p-12 bg-primary">
-        <h2 className="text-[#EFD830] font-bold text-3xl mb-6">About Us</h2>
-        <p className="w-full text-base leading-relaxed text-white md:w-2/3 md:text-xl">
-          S.T.A.R Union is a student activity founded on September 9, 2023, with
-          a specific focus on helping students in the field of technology,
-          particularly computer science. Its primary goals include spreading
-          awareness and encouragement regarding micro-business development and
-          the concept of startups among students and will achieve these goals
-          through various activities, such as workshops and events.
-        </p>
-      </div>
+      <ScrollAnimation>
+        <div className="flex flex-col items-center w-full p-8 text-center shadow-lg rounded-2xl md:p-12 bg-primary">
+          <h2 className="text-[#EFD830] font-bold text-3xl mb-6">About Us</h2>
+          <p className="w-full text-base leading-relaxed text-white md:w-2/3 md:text-xl">
+            S.T.A.R Union is a student activity founded on September 9, 2023,
+            with a specific focus on helping students in the field of
+            technology, particularly computer science. Its primary goals include
+            spreading awareness and encouragement regarding micro-business
+            development and the concept of startups among students and will
+            achieve these goals through various activities, such as workshops
+            and events.
+          </p>
+        </div>
+      </ScrollAnimation>
 
       <div className="grid w-full grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4 ">
-        {statsData.map((stat) => (
-          <div
-            key={stat.id}
-            className="flex flex-row items-center justify-center gap-4 px-6 py-5 text-white transition-transform rounded-lg shadow-md bg-primary hover:scale-105"
-          >
-            <div className="logo shrink-0">
-              <img
-                src={stat.icon}
-                alt={stat.text}
-                className="object-contain w-8 md:w-10"
-              />
+        {statsData.map((stat, index) => (
+          <ScrollAnimation mode="popLayout" delay={(index % 4) * 150}>
+            <div
+              key={stat.id}
+              className="flex flex-row items-center justify-center gap-4 px-6 py-5 text-white transition-transform rounded-lg shadow-md bg-primary hover:scale-105"
+            >
+              <div className="logo shrink-0">
+                <img
+                  src={stat.icon}
+                  alt={stat.text}
+                  className="object-contain w-8 md:w-10"
+                />
+              </div>
+              <div className="flex flex-col items-start justify-center">
+                <h2 className="font-bold text-2xl md:text-3xl text-[#EFD830] leading-none">
+                  {stat.number}
+                </h2>
+                <p className="text-sm md:text-base whitespace-nowrap">
+                  {stat.text}
+                </p>
+              </div>
             </div>
-            <div className="flex flex-col items-start justify-center">
-              <h2 className="font-bold text-2xl md:text-3xl text-[#EFD830] leading-none">
-                {stat.number}
-              </h2>
-              <p className="text-sm md:text-base whitespace-nowrap">
-                {stat.text}
-              </p>
-            </div>
-          </div>
+          </ScrollAnimation>
         ))}
       </div>
     </div>
--- src/features/home/components/HeroSection.jsx
+++ src/features/home/components/HeroSection.jsx
@@ -1,30 +1,31 @@
 import React from "react";
 import starLogo from "../../../assets/images/star_logo_2-removebg-preview 1.png";
 import { Button } from "@/components/ui/button";
+import ScrollAnimation from "@/components/ui/ScrollAnimation";
 
 export default function HeroSection() {
   return (
-    <div className="flex flex-col-reverse items-center justify-center gap-10 px-6 py-10 md:flex-row md:gap-20 md:px-20">
-      <div className="flex flex-col items-center justify-center flex-1 gap-5 text-center md:text-left">
-        <h2 className="text-[#EFD830] font-bold text-4xl md:text-5xl">
-          Reach The Stars
-        </h2>
-        <p className="text-lg text-gray-300 md:text-xl">
-          Unlock your potential. Build your future. Lead with confidence.
-        </p>
+    <div className="min-h-screen flex flex-col-reverse items-center justify-center gap-10 px-6 py-10 md:flex-row md:gap-60 md:px-20">
+      <ScrollAnimation variant="fade-right">
+        <div className="flex flex-col items-center justify-center flex-1 gap-5 text-center">
+          <h2 className="text-[#EFD830] font-bold text-3xl md:text-4xl lg:text-6xl">
+            Reach The Stars
+          </h2>
+          <p className="text-lg text-gray-300 md:text-xl lg:text-2xl max-w-lg leading-relaxed">
+            Unlock your potential. Build your future. Lead with confidence.
+          </p>
 
-        <Button className="bg-primary hover:bg-[#683ce3] text-white text-lg px-8 py-6 rounded">
-          Join Us
-        </Button>
-      </div>
+          <Button className="bg-primary hover:bg-[#683ce3] text-white text-xl px-8 py-7 rounded">
+            Join Us
+          </Button>
+        </div>
+      </ScrollAnimation>
 
-      <div className="flex justify-center flex-1 p-4 md:p-8 ">
-        <img
-          src={starLogo}
-          alt="Star Logo"
-          className="object-contain w-48 md:w-64"
-        />
-      </div>
+      <ScrollAnimation variant="fade-left">
+        <div className="flex justify-center md:justify-end items-center flex-1 p-4 md:p-8 w-full">
+          <img src={starLogo} alt="Star Logo" className="object-contain  w-72" />
+        </div>
+      </ScrollAnimation>
     </div>
   );
 }
