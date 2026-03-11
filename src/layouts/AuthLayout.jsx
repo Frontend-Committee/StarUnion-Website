58c14b8a205bd3cb9d72d4170f2c6e32b95d19b6
MariamYasser227 <yassermariam444@gmail.com>
2026-03-11 20:28:27 +0200

    feat(UI): add loading and error components and integrate with query

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,7 +1,7 @@
 import { Link, Outlet, useNavigation } from "react-router-dom";
-import Logo from '../assets/icons/StarLogo.png';
-import bg from '../assets/images/WhatsApp Image 2026-02-23 at 3.23.19 PM 1.png';
-import LoadingSpinner from '../components/ui/LoadingSpinneer';
+import Logo from "../assets/icons/StarLogo.png";
+import bg from "../assets/images/WhatsApp Image 2026-02-23 at 3.23.19 PM 1.png";
+import LoadingSpinner from "../components/ui/LoadingSpinneer";
 
 export default function AuthLayout({ children }) {
   const navigation = useNavigation();
@@ -11,7 +11,9 @@ export default function AuthLayout({ children }) {
     <div className="relative flex flex-col min-h-screen">
       {isNavigating && <LoadingSpinner fullScreen={true} />}
 
-      <main className={`flex-1 relative ${isNavigating ? "opacity-50 pointer-events-none" : "opacity-100"}`}>
+      <main
+        className={`flex-1 relative ${isNavigating ? "opacity-50 pointer-events-none" : "opacity-100"}`}
+      >
         {/* Background image as <img> for native sharpness */}
         <img
           src={bg}
@@ -24,8 +26,15 @@ export default function AuthLayout({ children }) {
         {/* Overlay + content container with improved scrolling */}
         <div className="absolute inset-0 bg-[#311f62]/60 overflow-y-auto overflow-x-hidden">
           <div className="flex flex-col items-center justify-start min-h-full p-4 py-20 md:justify-center md:py-10">
-            <Link to="/" className="fixed z-50 cursor-pointer md:absolute top-6 left-6 md:top-8 md:left-8">
-              <img src={Logo} alt="Logo" className="transition-transform rounded-full shadow-lg size-11 md:size-12 hover:scale-110 active:scale-95 bg-white/10 backdrop-blur-sm" />
+            <Link
+              to="/"
+              className="fixed z-50 cursor-pointer md:absolute top-6 left-6 md:top-8 md:left-8"
+            >
+              <img
+                src={Logo}
+                alt="Logo"
+                className="transition-transform rounded-full shadow-lg size-11 md:size-12 hover:scale-110 active:scale-95 bg-white/10 backdrop-blur-sm"
+              />
             </Link>
 
             <div className="flex justify-center w-full mt-4">
